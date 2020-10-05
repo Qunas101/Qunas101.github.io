41b23ded00ef783f78bddefc992b22620ab7dd7e
Qunas101 <40661883+Qunas101@users.noreply.github.com>
2020-10-05 19:06:04 +0300

    Added display

--- sketch.js
+++ sketch.js
@@ -1,13 +1,39 @@
+let myFont;
+function preload() {
+	myFont = loadFont('04B_30__.TTF');
+}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	frameRate(120);
 }
 
 function draw() {
+	translate(windowWidth/2, windowHeight/2);
 	let hr = map(hour(), 0, 23, 0, 255);
 	let min = map(minute(), 0, 59, 0, 255);
 	let sec = map(second(), 0, 59, 0, 255);
 	background(hr, min, sec);
+
+	let hrF = hour();
+	let minF = minute();
+	let secF = second();
+	if(hour() < 10){
+		hrF = '0' + hour().toString();
+	}
+	if(minute() < 10){
+		minF = '0' + minute().toString();
+	}
+	if(second() < 10){
+		secF = '0' + second().toString();
+	}
+	let x = (hr+min+sec)/3;
+	textSize(80);
+	textFont(myFont);
+	textAlign(CENTER);
+	fill(255-x, 255-x, 255-x);
+	stroke(255-hr, 255-min, 255-sec);
+	strokeWeight(8);
+	text(hrF + ':' + minF + ':' + secF, 0, 0);
 }
 
 function windowResized() {
