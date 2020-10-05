9e2bbfbd11079b2489ac8c34ae9a8962b64229ed
Qunas101 <40661883+Qunas101@users.noreply.github.com>
2020-10-05 10:24:01 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-	createCanvas(windowHeight, windowWidth);
+	createCanvas(windowWidth, windowHeight);
 	frameRate(120);
 }
 
 function draw() {
-	x = map(mouseX, 0, windowHeight, 0, 255);
-	y = map(mouseY, 0, windowWidth, 0, 255);
+	x = map(mouseX, 0, windowWidth, 0, 255);
+	y = map(mouseY, 0, windowHeight, 0, 255);
 	background(x, y, 0);
 }
